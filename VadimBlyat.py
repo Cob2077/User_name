9238056abb7d178cb6fe4143db0b5bb4014d3a45
Sz. Levente <96853166+Cob2077@users.noreply.github.com>
2022-10-24 16:21:26 +0200

    Add files via upload

---
+++ VadimBlyat.py
@@ -0,0 +1,15 @@
+print("what is your name?")
+user_name = input()
+
+if user_name == "Vadim":
+    print("Vadim blyat")
+
+elif user_name == "Boris":
+    print("Cheeki Breeki")
+
+elif user_name == "vadim":
+    print("Vadim blyat")
+    
+else:
+    print("hello," + user_name)
+input("press Eneter key to exit")
